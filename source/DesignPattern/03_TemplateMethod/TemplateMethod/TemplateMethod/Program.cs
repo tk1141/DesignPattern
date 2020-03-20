b05df64999532dd38f9cb444d44a35bda89e1794
Gogs <gogs@fake.local>
2020-03-20 18:03:26 +0900

    3 TemplateMethodの実装

---
+++ source/DesignPattern/03_TemplateMethod/TemplateMethod/TemplateMethod/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TemplateMethod {
+    class Program {
+        static void Main(string[] args) {
+            WoodCutPrint wcp = new TanakasWoodCutPrint();
+            wcp.createWoodCutPrint();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ source/DesignPattern/03_TemplateMethod/TemplateMethod/TemplateMethod/TanakasWoodCutPrint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplateMethod {
+    class TanakasWoodCutPrint : WoodCutPrint {
+        public override void cut(Cuttable hanzai) {
+            Console.WriteLine("hanzai にマジックを使って大好きな女の子の絵を描く");
+        }
+
+        public override void draw(Cuttable hanzai) {
+            Console.WriteLine("彫刻刀を使って細部まで丁寧に hanzai を彫る");
+        }
+
+        public override void print(Cuttable hanzai) {
+            Console.WriteLine("版画インクと馬簾を使って豪快にプリントする");
+        }
+    }
+}
