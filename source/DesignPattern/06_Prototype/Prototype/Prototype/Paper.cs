30c7a0dde80d4fcc69c0fd975e74f304fdf1513d
Gogs <gogs@fake.local>
2020-03-20 19:08:01 +0900

    6 Prototypeの実装

---
+++ source/DesignPattern/06_Prototype/Prototype/Prototype/Paper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype {
+    public class Paper : Cloneable{
+        private string name;
+
+        public Paper() { }
+
+        public Paper(string name) {
+            this.name = name;
+        }
+
+        public Cloneable createClone() {
+            Paper newPaper = new Paper();
+            newPaper.name = this.name;
+            //newPaperとthisを重ねて、thisの形に切り抜く。
+            return newPaper;
+        }
+    }
+}
---
+++ source/DesignPattern/06_Prototype/Prototype/Prototype/Teacher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype {
+    public class Teacher {
+        public Paper[] CreateManyCrystals() {
+
+            Paper prototype = new Paper("雪の結晶");
+            drawCrystal(prototype);
+            cutAccordanceWithLine(prototype);
+
+            Paper[] papers = new Paper[100];
+            for (int n = 0; n < papers.Length; n++) {
+                papers[n] = (Paper)prototype.createClone();
+            }
+
+            return papers;
+        }
+
+        private void drawCrystal(Paper paper) {
+            // 描いたり消したりしながら、きれいに描くため時間がかかる
+        }
+        private void cutAccordanceWithLine(Paper paper) {
+            // 描かれた線に沿ってきれいに切るには時間がかかる
+        }
+
+    }
+}
